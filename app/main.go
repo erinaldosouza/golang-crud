9a310e943a1b0ba81ced46e0dfbe13396162fcfd
Erinaldo Souza <erinaldo.sds@gmail.com>
2024-06-22 11:04:13 -0300

    Refactory main.go

--- app/main.go
+++ app/main.go
@@ -8,11 +8,26 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main() {
+const APP_NAME = "APP_NAME"
 
-	if godotenv.Load() != nil {
-		log.Fatal("Error loading .env file")
+// loadEnv loads environment variables from a .env file.
+func loadEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
 	}
-	fmt.Println(os.Getenv("APP_NAME"))
+}
 
+// getEnv retrieves the value of the environment variable named by the key.
+// It returns the value, which will be empty if the variable is not present.
+func getEnv(key string) string {
+	return os.Getenv(key)
+}
+
+func main() {
+	loadEnv()
+	appName := getEnv("APP_NAME")
+	if appName == "" {
+		log.Fatal("APP_NAME environment variable not set")
+	}
+	fmt.Println(appName)
 }
